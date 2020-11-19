78cf85b8ae060df51fba268c00eff60305a2c67d
Dima Voichuck <dvoichuck@e1r8p1.unit.ua>
2020-11-19 16:53:46 +0200

    modified if else in parse function

--- src/mx_parse_ush_manager.c
+++ src/mx_parse_ush_manager.c
@@ -6,21 +6,26 @@
 
 static void parse_str_input(t_ush *ush) {
     int i = mx_count_words(ush->str_input, ' ');
-
+//    mx_create_node()
 }
 
 void mx_parse_ush_manager(t_ush *ush) {
-    parse_str_input(ush);
     char *str_del_char = mx_del_extra_spaces(ush->str_input);
-    if (ush->str_input[0] == '\n' || str_del_char[0] == '\0')
+
+    if (str_del_char[0] == '\0')
         mx_printstr("");
-    else if (mx_strcmp(ush->str_input, "exit\n") == 0) {
-        mx_printstr("exit\n");
-        exit(1);
-    }
     else {
-        mx_printerr("ush: command not found: ");
-        mx_printerr(ush->str_input);
+        parse_str_input(ush);
+        if (ush->str_input[0] == '\n')
+            mx_printstr("");
+        else if (mx_strcmp(ush->str_input, "exit\n") == 0) {
+            mx_printstr("exit\n");
+            exit(1);
+        }
+        else {
+            mx_printerr("ush: command not found: ");
+            mx_printerr(ush->str_input);
+        }
     }
     free(ush->str_input);
     ush->str_input = NULL;
